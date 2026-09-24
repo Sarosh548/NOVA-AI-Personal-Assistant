83fcc37e39eb83ad83359a2b09e9a390d42812bd
Sarosh548 <sarosh548548@gmail.com>
2026-09-24 17:52:07 +0500

    fix: correct notification retention migration merge heads

--- backend/migrations/versions/d3f1e9a7c2b4_merge_notification_retention_heads.py
+++ backend/migrations/versions/d3f1e9a7c2b4_merge_notification_retention_heads.py
@@ -1,7 +1,7 @@
 """merge notification delivery retention with existing migration heads
 
 Revision ID: d3f1e9a7c2b4
-Revises: f7c2a91d4e63, 7c9e2f4a1b6d, c9e2a5b7d1f4
+Revises: 7c9e2f4a1b6d, c9e2a5b7d1f4
 Create Date: 2026-09-24 12:40:00.000000
 
 """
@@ -11,7 +11,6 @@ from typing import Sequence, Union
 
 revision: str = "d3f1e9a7c2b4"
 down_revision: Union[str, Sequence[str], None] = (
-    "f7c2a91d4e63",
     "7c9e2f4a1b6d",
     "c9e2a5b7d1f4",
 )
